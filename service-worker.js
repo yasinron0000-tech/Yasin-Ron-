686c7d536a1afc6d8d597ed8ab122d32ae00b08a
yasinron0000-tech <yasinron0000@gmail.com>
2026-08-14 20:42:16 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,11 @@
-const CACHE_VERSION = 'excel-master-yasin-v12';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './icon.svg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_VERSION)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  if (request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_VERSION).then(cache => cache.put(request, copy)).catch(() => {});
-        return response;
-      })
-      .catch(() => caches.match(request).then(cached => cached || caches.match('./index.html')))
-  );
+const CACHE_VERSION = "excel-master-yasin-v14";
+const BASE = "/Yasin-Ron-/";
+const APP_SHELL = [BASE, BASE+"index.html", BASE+"manifest.json", BASE+"icon-192.png", BASE+"icon-512.png", BASE+"icon.svg"];
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_VERSION).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch", e => {
+  if(e.request.method!=="GET") return;
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin) return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone(); caches.open(CACHE_VERSION).then(x=>x.put(e.request,c)).catch(()=>{}); return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match(BASE+"index.html"))));
 });
