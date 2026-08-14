6205401bdc235b063bb77e6e265526d431967725
yasinron0000-tech <yasinron0000@gmail.com>
2026-08-14 21:01:58 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-const CACHE_VERSION='excel-master-yasin-ron-v12';
-self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='excel-master-pwa-v1';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
 self.addEventListener('fetch',e=>{
-  if(e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+ if(e.request.method!=='GET') return;
 });
